83bb907719e191df922fae379788b4cb077590f7
Tomasz Łuczak <tluczak.dev@gmail.com>
2024-04-24 10:42:16 +0200

    back to start

--- src/main/java/org/example/model/Match.java
+++ src/main/java/org/example/model/Match.java
@@ -8,33 +8,15 @@ import static java.time.LocalDateTime.now;
 public class Match {
 
     private final String id = UUID.randomUUID().toString();
-    private final LocalDateTime startMatch = now();
-    private LocalDateTime endMatch;
 
     private int homeScore = 0;
     private int awayScore = 0;
 
-    private Team homeTeam;
-    private Team awayTeam;
-
     private Match() {
-    }
-
-    public Match(Team homeTeam, Team awayTeam) {
-        this.homeTeam = homeTeam;
-        this.awayTeam = awayTeam;
     }
 
     public String getId() {
         return id;
-    }
-
-    public LocalDateTime getStartMatch() {
-        return startMatch;
-    }
-
-    public LocalDateTime getEndMatch() {
-        return endMatch;
     }
 
     public int getHomeScore() {
@@ -43,25 +25,5 @@ public class Match {
 
     public int getAwayScore() {
         return awayScore;
-    }
-
-    public Team getHomeTeam() {
-        return homeTeam;
-    }
-
-    public Team getAwayTeam() {
-        return awayTeam;
-    }
-
-    public void increaseHomeScore(){
-        this.homeScore += 1;
-    }
-
-    public void increaseAwayScore(){
-        this.awayScore += 1;
-    }
-
-    public void endMatch(){
-        this.endMatch = now();
     }
 }
--- src/main/java/org/example/service/ITeamService.java
+++
@@ -1,8 +0,0 @@
-package org.example.service;
-
-import org.example.model.Team;
-
-public interface ITeamService {
-
-    Team createTeam(String name);
-}
